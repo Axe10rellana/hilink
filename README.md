35a24aa1cf31fd21b7e653080604d433358b5718
Axe10rellana <axelorellana550@gmail.com>
2024-11-11 16:21:09 -0300

    hilink

---
+++ README.md
@@ -0,0 +1,58 @@
+# Hilink
+
+## Descripción
+
+Código base de la Interfaz Hilink Hecha en Next, React.js y Tailwind CSS.
+
+---
+
+## Vista En Versión Desktop
+
+![Vista_En_Versión_Desktop](public/design/desktop-design.jpg)
+
+## Vista En Versión Mobile
+
+![Vista_En_Versión_Mobile](public/design/mobile-design.jpg)
+
+---
+
+## Enlace A La Aplicación
+
+- [Hilink](https://hilink-modern-ui.netlify.app/)
+
+---
+
+## Hecho con
+
+- [React](https://react.dev/) - JS library
+- [Next](https://nextjs.org/) - React Framework
+- [Typescript](https://www.typescriptlang.org/) - For Javascript with syntax for types
+- [Tailwind CSS](https://tailwindcss.com/) - For styles
+
+---
+
+## Comandos
+
+- npm i
+- npm run dev
+- npm run build
+
+---
+
+## Dependencias
+
+- Next
+- React
+
+## Dependencias De Desarrollo
+
+- Autoprefixer
+- Post CSS
+- Tailwind CSS
+- Typescript
+
+---
+
+## Autor
+
+- Website - [Axe10rellana](https://axe10rellana.github.io/portafolio/portafolio/)
---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+//next
+import type { Metadata } from "next";
+
+//styles
+import "./globals.css";
+
+//componets
+import { Footer, Navbar } from "../components";
+
+export const metadata: Metadata = {
+  title: "Hilink",
+  description: "Código base de la Interfaz Hilink Hecha en Next, React.js y Tailwind CSS.",
+  themeColor: "#ffffff",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      sizes: "16x16",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "favicon",
+      type: "image/x-icon",
+      sizes: "16x16",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon.ico",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden select-none">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
